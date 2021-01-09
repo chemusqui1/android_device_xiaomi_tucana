85026ad0f4d677620caabb65f0a8e2edb21192c9
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2021-01-09 19:21:53 +0100

    Update evolution_tucana.mk

--- evolution_tucana.mk
+++ evolution_tucana.mk
@@ -8,7 +8,7 @@ $(call inherit-product, device/xiaomi/tucana/device.mk)
 
 # Inherit some common EvolutionX stuff.
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
-EVO_BUILD_TYPE := OFFICIAL
+#EVO_BUILD_TYPE := UNOFFICIAL
 EXTRA_FOD_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 WITH_GAPPS := true
