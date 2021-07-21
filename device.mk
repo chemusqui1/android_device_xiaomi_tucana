bcc0259be5f754b7e7d1db4700ef6b7e9982d860
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2021-07-21 21:49:31 +0200

    Remove not needed stuff

--- device.mk
+++ device.mk
@@ -84,17 +84,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-# Partitions
-PRODUCT_BUILD_SUPER_PARTITION := false
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
 # Power
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-
-# Recovery
-PRODUCT_PACKAGES += \
-    fastbootd
 
 # Sensors
 PRODUCT_PACKAGES += \
